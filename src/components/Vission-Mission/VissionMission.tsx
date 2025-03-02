798027c46bf338d47cfca8369c0f3cb774f41694
Souhardya20 <souhardyadeb2016@gmail.com>
2025-03-02 12:28:23 +0530

    update

--- src/components/Vission-Mission/VissionMission.tsx
+++ src/components/Vission-Mission/VissionMission.tsx
@@ -13,53 +13,72 @@ const VisionMission: React.FC = () => {
 			/>
 			{/* About Details */}
 			<div className="flex p-14 flex-col gap-10 items-center justify-normal">
-				<div className="">
-					<div className="flex items-center gap-2">
-						<div className="border-2 rounded border-blue-400 h-10"></div>
-						<h3 className="text-[#364150] font-semibold uppercase text-[20px] lg:text-[24px]">
-							Vission
-						</h3>
+				<div className="flex items-center justify-center">
+					<div className="">
+						<div className="flex items-center gap-2">
+							<div className="border-2 rounded border-blue-400 h-10"></div>
+							<h3 className="text-[#364150] font-semibold uppercase text-[20px] lg:text-[24px]">
+								Vission
+							</h3>
+						</div>
+						<ul className="text-[14px] mt-4 list-disc px-10  max-w-fit w-[95%] lg:text-[16px] space-y-4">
+							<li>
+								To emerge as a strong co-ordination and healthy interaction
+								between the college and its Alumni.
+							</li>
+							<li>
+								To promote development of healthy socio-technical interaction
+								with ethical values for creative engineering solutions
+								commensurate with global challenges
+							</li>
+						</ul>
 					</div>
-					<ul className="text-[14px] mt-4 list-disc px-10  max-w-fit w-[90%] lg:text-[16px] space-y-4">
-						<li>
-							To emerge as a strong co-ordination and healthy interaction
-							between the college and its Alumni.
-						</li>
-						<li>
-							To promote development of healthy socio-technical interaction with
-							ethical values for creative engineering solutions commensurate
-							with global challenges
-						</li>
-					</ul>
+					<Image
+						src="/assets/JGEC-Logo.jpg"
+						alt=""
+						height={250}
+						width={250}
+						className="w-[40%] pr-8 pt-8"
+					/>
 				</div>
-				<div className="">
-					<div className="flex items-center gap-2">
-						<div className="border-2 rounded border-blue-400 h-10"></div>
-						<h3 className="text-[#364150] font-semibold uppercase text-[20px] lg:text-[24px]">
-							Mission
-						</h3>
+
+				<div className="flex items-center justify-center">
+					<Image
+						src="/assets/JGECAA.jpg"
+						alt=""
+						height={250}
+						width={250}
+						className="w-[40%] pr-8 pt-8"
+					/>
+					<div className="">
+						<div className="flex items-center gap-2">
+							<div className="border-2 rounded border-blue-400 h-10"></div>
+							<h3 className="text-[#364150] font-semibold uppercase text-[20px] lg:text-[24px]">
+								Mission
+							</h3>
+						</div>
+						<ul className="text-[14px] mt-4 text-clip list-disc px-10 max-w-fit  lg:text-[16px] space-y-4">
+							<li>
+								Fostering an inspiring and conductive learning environment to
+								prepare skilled and competent engineers and entrepreneurs for
+								sustainable development of the society.
+							</li>
+							<li>
+								Imparting innovative educational process through Bridging the
+								Gap program for meeting the growing challenges of between
+								Academia and Industry.
+							</li>
+							<li>
+								Creating a knowledge centre of advanced technologies committed
+								to societal growth using environment-friendly technologies as
+								far as practicable.
+							</li>
+							<li>
+								Distributing study materials and extending assistances to poor
+								and or needy students as far as permissible.
+							</li>
+						</ul>
 					</div>
-					<ul className="text-[14px] mt-4 text-clip list-disc px-10 max-w-fit w-[90%] lg:text-[16px] space-y-4">
-						<li>
-							Fostering an inspiring and conductive learning environment to
-							prepare skilled and competent engineers and entrepreneurs for
-							sustainable development of the society.
-						</li>
-						<li>
-							Imparting innovative educational process through Bridging the Gap
-							program for meeting the growing challenges of between Academia and
-							Industry.
-						</li>
-						<li>
-							Creating a knowledge centre of advanced technologies committed to
-							societal growth using environment-friendly technologies as far as
-							practicable.
-						</li>
-						<li>
-							Distributing study materials and extending assistances to poor and
-							or needy students as far as permissible.
-						</li>
-					</ul>
 				</div>
 			</div>
 			{/*  */}
